3ef1f0f754f2a9cb1e8a1aed5f5950b287a840fe
Curtis Mason <cumason@bu.edu>
2019-11-15 15:56:55 -0500

    Added horizontal and vertical victory checking

--- src/GameBoards/connect4.py
+++ src/GameBoards/connect4.py
@@ -1,7 +1,13 @@
 import numpy as np
+STANDARD_CONNECT_FOUR_SIZE = (6, 7)
 class Board():
 	def __init__(self, num_rows, num_cols):
 		self.board = np.zeros([num_rows, num_cols])
+		self.action_space = num_cols
+
+	def shape(self):
+		# Returns shape of board
+		return self.board.shape
 
 	def winner_exists(self):
 		"""
@@ -27,8 +33,26 @@ class Board():
 
 		return False, -1
 
+	def clear(self):
+		"""
+		Removes all tokens frmo the board
+		"""
+		self.board = np.zeros(self.board.shape)
+		self.action_space = self.board.shape[1]
+
 	def won_horizontal(self):
+		for row in self.board:
+			for i in range(len(row)):
+				if i+3 > len(row) - 1:
+					continue
+				elif row[i] == row[i+1] == row[i+2] == row[i+3] != 0:
+					return True, row[i]
 		return False, -1
 
 	def won_vertical(self):
-		return False, -1
+		try:
+			self.board = self.board.T
+			return self.won_horizontal()
+		finally:
+			self.board = self.board.T
+
--- src/GameBoards/test_connect4.py
+++ src/GameBoards/test_connect4.py
@@ -1,4 +1,38 @@
-from GameBoards.connect4 import Board
-def test_board():
-	b = Board(5,5)
-	assert(b.board.shape == (5,5))
+from GameBoards.connect4 import Board, STANDARD_CONNECT_FOUR_SIZE
+def test_board_shape():
+	print('Beginning Test for Board Shape')
+	# Tests shape of board is true
+	b = Board(*STANDARD_CONNECT_FOUR_SIZE)
+	# Test board shape
+	print('Board Shape Should be {0}, found: {1}'.format(STANDARD_CONNECT_FOUR_SIZE, b.shape()))
+	assert(b.shape() == STANDARD_CONNECT_FOUR_SIZE)
+
+	# Test action space
+	print('Action Space Should be {0}, found: {1}'.format(STANDARD_CONNECT_FOUR_SIZE[1], b.action_space))
+	assert(b.action_space == STANDARD_CONNECT_FOUR_SIZE[1])
+
+
+def test_board_won_horizontal_or_vertical():
+	print('Beginning Test for horizontal and vertical wins')
+	b = Board(*STANDARD_CONNECT_FOUR_SIZE)
+	# Iterate returns true on every possible win condition
+	for i, row in enumerate(b.board):
+		for col in range(len(row))[:-4]:
+
+			# Vertical Test
+			if i <= len(b.board) - 4:
+				b.board[i][col] = b.board[i+1][col] = b.board[i+2][col] = b.board[i+3][col] = 1
+				won, player = b.won_vertical()
+				print("Expect to find (True, 1), found: {0}".format((won, player)))
+				assert(won and player == 1)
+				b.clear()
+
+			# Horizontal Test
+			b.board[i][col] = b.board[i][col + 1] = b.board[i][col + 2] = b.board[i][col + 3] = 1
+			won, player = b.won_horizontal()
+			print("Expect to find (True, 1), found: {0}".format((won, player)))
+			assert(won and player == 1)
+			b.clear()
+
+	won, player = b.won_vertical()
+	assert(not won and player == -1)
