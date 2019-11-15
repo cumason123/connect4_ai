947f1824733a6366a491545dd3beb2f4c48905ca
Curtis Mason <cumason@bu.edu>
2019-11-15 17:28:05 -0500

    Changed indentation to spaces

--- src/GameBoard/connect4.py
+++ src/GameBoard/connect4.py
@@ -3,168 +3,168 @@ from typing import Dict, Tuple, Sequence
 
 STANDARD_CONNECT_FOUR_SIZE = (6, 7)
 class Board():
-	def __init__(self, num_rows: int, num_cols: int):
-		assert(num_rows > 4 and num_cols > 4)
-		self.board = np.zeros([num_rows, num_cols]).astype(int)
-		self.action_space = num_cols
+    def __init__(self, num_rows: int, num_cols: int):
+        assert(num_rows > 4 and num_cols > 4)
+        self.board = np.zeros([num_rows, num_cols]).astype(int)
+        self.action_space = num_cols
 
 
-	def shape(self) -> Tuple[int, int]:
-		"""
-		Returns current shape of board
+    def shape(self) -> Tuple[int, int]:
+        """
+        Returns current shape of board
 
-		Returns
-		----------
-		(num_rows : int, num_cols : int)
-		"""
-		return self.board.shape
-
-
-	def apply_action(self, action: int, player: int):
-		"""
-		Puts player token in a column.
-
-		Parameters
-		----------
-		action : int
-			action specifying which column to place token in
-		player : int
-			player id of the token
-		"""
-		print(action)
-		print(self)
-		assert(action < self.action_space and self.board[-1][action] == 0)
-		for row in range(self.shape()[0]):
-			if self.board[row][action] == 0:
-				self.board[row][action] = player
-				return
-
-	def winner_exists(self) -> Tuple[bool, int]:
-		"""
-		Checks to see if a player has won. If no players have won, return false and -1
-
-		Returns
-		----------
-		(bool, int)
-			returns a tuple where first element is true if someone won and second element
-			specifies which player won
-		"""
-		won, player = self.won_horizontal()
-		if won:
-			print('Found Horizontal Win')
-			return won, player
-
-		won, player = self.won_vertical()
-		if won:
-			print('Found Vertical Win')
-			return won, player
-
-		won, player = self.won_diagonal()
-		if won:
-			print('Found Diagonal Win')
-			return won, player
-
-		return False, -1
+        Returns
+        ----------
+        (num_rows : int, num_cols : int)
+        """
+        return self.board.shape
 
 
-	def clear(self):
-		"""
-		Removes all tokens frmo the board
-		"""
-		self.board = np.zeros(self.board.shape)
-		self.action_space = self.board.shape[1]
+    def apply_action(self, action: int, player: int):
+        """
+        Puts player token in a column.
+
+        Parameters
+        ----------
+        action : int
+            action specifying which column to place token in
+        player : int
+            player id of the token
+        """
+        print(action)
+        print(self)
+        assert(action < self.action_space and self.board[-1][action] == 0)
+        for row in range(self.shape()[0]):
+            if self.board[row][action] == 0:
+                self.board[row][action] = player
+                return
+
+    def winner_exists(self) -> Tuple[bool, int]:
+        """
+        Checks to see if a player has won. If no players have won, return false and -1
+
+        Returns
+        ----------
+        (bool, int)
+            returns a tuple where first element is true if someone won and second element
+            specifies which player won
+        """
+        won, player = self.won_horizontal()
+        if won:
+            print('Found Horizontal Win')
+            return won, player
+
+        won, player = self.won_vertical()
+        if won:
+            print('Found Vertical Win')
+            return won, player
+
+        won, player = self.won_diagonal()
+        if won:
+            print('Found Diagonal Win')
+            return won, player
+
+        return False, -1
 
 
-	def won_horizontal(self) -> Tuple[bool, int]:
-		"""
-		Checks every 4 horizontal consecutive slots to see if someone occupies them. If someone
-		does, we will return True and that player.
-
-		Returns
-		----------
-		(bool, int)
-			returns a tuple where first element is true if someone won and second element
-			specifies which player won
-		"""
-		for row in self.board:
-			for i in range(len(row)):
-				if i+3 > len(row) - 1:
-					continue
-				elif row[i] == row[i+1] == row[i+2] == row[i+3] != 0:
-					return True, row[i]
-		return False, -1
+    def clear(self):
+        """
+        Removes all tokens frmo the board
+        """
+        self.board = np.zeros(self.board.shape)
+        self.action_space = self.board.shape[1]
 
 
-	def won_vertical(self) -> Tuple[bool, int]:
-		"""
-		Checks every 4 vertical consecutive slots to see if someone occupies them. If someone
-		does, we will return True and that player.
+    def won_horizontal(self) -> Tuple[bool, int]:
+        """
+        Checks every 4 horizontal consecutive slots to see if someone occupies them. If someone
+        does, we will return True and that player.
 
-		Algorithm
-		----------
-		Numpy transposition happens in constant time therefore it is more optimal to simply
-		run won_horizontal on the transposed matrix. 
-
-		Returns
-		----------
-		(bool, int)
-			returns a tuple where first element is true if someone won and second element
-			specifies which player won
-		"""
-		try:
-			self.board = self.board.T
-			return self.won_horizontal()
-		finally:
-			self.board = self.board.T
+        Returns
+        ----------
+        (bool, int)
+            returns a tuple where first element is true if someone won and second element
+            specifies which player won
+        """
+        for row in self.board:
+            for i in range(len(row)):
+                if i+3 > len(row) - 1:
+                    continue
+                elif row[i] == row[i+1] == row[i+2] == row[i+3] != 0:
+                    return True, row[i]
+        return False, -1
 
 
-	def won_diagonal(self) -> Tuple[bool, int]:
-		"""
-		Checks every 4 diagonal consecutive slots to see if someone occupies them. If someone
-		does, we will return True and that player.
+    def won_vertical(self) -> Tuple[bool, int]:
+        """
+        Checks every 4 vertical consecutive slots to see if someone occupies them. If someone
+        does, we will return True and that player.
 
-		Returns
-		----------
-		(bool, int)
-			returns a tuple where first element is true if someone won and second element
-			specifies which player won
-		"""
-		for x in range(self.shape()[0]):
-			for y in range(self.shape()[1]):
-			
-				if (x+3 < self.shape()[0] and y+3 < self.shape()[1]):
-					if self.board[x][y] == self.board[x+1][y+1] == \
-						self.board[x+2][y+2] == self.board[x+3][y+3] != 0:
-						return True, self.board[x][y]
-				if (x-3 >= 0 and y-3 >= 0):
-					if self.board[x][y] == self.board[x-1][y-1] == \
-						self.board[x-2][y-2] == self.board[x-3][y-3] != 0:
-						return True, self.board[x][y]
-		return False, -1
+        Algorithm
+        ----------
+        Numpy transposition happens in constant time therefore it is more optimal to simply
+        run won_horizontal on the transposed matrix. 
+
+        Returns
+        ----------
+        (bool, int)
+            returns a tuple where first element is true if someone won and second element
+            specifies which player won
+        """
+        try:
+            self.board = self.board.T
+            return self.won_horizontal()
+        finally:
+            self.board = self.board.T
 
 
-	def is_full(self) -> bool:
-		"""
-		Returns whether connect four board is full
+    def won_diagonal(self) -> Tuple[bool, int]:
+        """
+        Checks every 4 diagonal consecutive slots to see if someone occupies them. If someone
+        does, we will return True and that player.
 
-		Returns
-		----------
-		bool
-		"""
-		for row in self.board:
-			for col in row:
-				if col == 0:
-					return False
-		return True
+        Returns
+        ----------
+        (bool, int)
+            returns a tuple where first element is true if someone won and second element
+            specifies which player won
+        """
+        for x in range(self.shape()[0]):
+            for y in range(self.shape()[1]):
+            
+                if (x+3 < self.shape()[0] and y+3 < self.shape()[1]):
+                    if self.board[x][y] == self.board[x+1][y+1] == \
+                        self.board[x+2][y+2] == self.board[x+3][y+3] != 0:
+                        return True, self.board[x][y]
+                if (x-3 >= 0 and y-3 >= 0):
+                    if self.board[x][y] == self.board[x-1][y-1] == \
+                        self.board[x-2][y-2] == self.board[x-3][y-3] != 0:
+                        return True, self.board[x][y]
+        return False, -1
 
-	def __str__(self):
-		"""Returns grid as string form"""
-		s = '-'*(len(self.board[0])*4) + '\n'
-		s += '| '
-		for row in self.board:
-			for col in row:
-				s += str(col) + ' | '
-			s = s[:-3]
-			s += '\n'
-			s += '-'*len(row)*4 + '\n| '
-		return s[:-2]
+
+    def is_full(self) -> bool:
+        """
+        Returns whether connect four board is full
+
+        Returns
+        ----------
+        bool
+        """
+        for row in self.board:
+            for col in row:
+                if col == 0:
+                    return False
+        return True
+
+    def __str__(self):
+        """Returns grid as string form"""
+        s = '-'*(len(self.board[0])*4) + '\n'
+        s += '| '
+        for row in self.board:
+            for col in row:
+                s += str(col) + ' | '
+            s = s[:-3]
+            s += '\n'
+            s += '-'*len(row)*4 + '\n| '
+        return s[:-2]
--- src/GameBoard/test_connect4.py
+++ src/GameBoard/test_connect4.py
@@ -1,64 +1,64 @@
 from GameBoard.connect4 import Board, STANDARD_CONNECT_FOUR_SIZE
 def test_board_shape():
-	print('Beginning Test for Board Shape')
-	# Tests shape of board is true
-	b = Board(*STANDARD_CONNECT_FOUR_SIZE)
+    print('Beginning Test for Board Shape')
+    # Tests shape of board is true
+    b = Board(*STANDARD_CONNECT_FOUR_SIZE)
 
-	# Test board shape
-	print('Board Shape Should be {0}, found: {1}'.format(STANDARD_CONNECT_FOUR_SIZE, b.shape()))
-	assert(b.shape() == STANDARD_CONNECT_FOUR_SIZE)
+    # Test board shape
+    print('Board Shape Should be {0}, found: {1}'.format(STANDARD_CONNECT_FOUR_SIZE, b.shape()))
+    assert(b.shape() == STANDARD_CONNECT_FOUR_SIZE)
 
-	# Test action space
-	print('Action Space Should be {0}, found: {1}'.format(STANDARD_CONNECT_FOUR_SIZE[1], b.action_space))
-	assert(b.action_space == STANDARD_CONNECT_FOUR_SIZE[1])
+    # Test action space
+    print('Action Space Should be {0}, found: {1}'.format(STANDARD_CONNECT_FOUR_SIZE[1], b.action_space))
+    assert(b.action_space == STANDARD_CONNECT_FOUR_SIZE[1])
 
 
 def test_board_won_horizontal_or_vertical():
-	print('Beginning Test for horizontal and vertical wins')
-	b = Board(*STANDARD_CONNECT_FOUR_SIZE)
-	# Iterate returns true on every possible win condition
-	for i, row in enumerate(b.board):
-		for col in range(len(row))[:-4]:
+    print('Beginning Test for horizontal and vertical wins')
+    b = Board(*STANDARD_CONNECT_FOUR_SIZE)
+    # Iterate returns true on every possible win condition
+    for i, row in enumerate(b.board):
+        for col in range(len(row))[:-4]:
 
-			# Vertical Test
-			if i <= len(b.board) - 4:
-				b.board[i][col] = b.board[i+1][col] = b.board[i+2][col] = b.board[i+3][col] = 1
-				won, player = b.winner_exists()
-				assert(won and player == 1)
-				b.clear()
+            # Vertical Test
+            if i <= len(b.board) - 4:
+                b.board[i][col] = b.board[i+1][col] = b.board[i+2][col] = b.board[i+3][col] = 1
+                won, player = b.winner_exists()
+                assert(won and player == 1)
+                b.clear()
 
-			# Horizontal Test
-			b.board[i][col] = b.board[i][col + 1] = b.board[i][col + 2] = b.board[i][col + 3] = 1
-			won, player = b.winner_exists()
-			assert(won and player == 1)
-			b.clear()
+            # Horizontal Test
+            b.board[i][col] = b.board[i][col + 1] = b.board[i][col + 2] = b.board[i][col + 3] = 1
+            won, player = b.winner_exists()
+            assert(won and player == 1)
+            b.clear()
 
-	won, player = b.winner_exists()
-	assert(not won and player == -1)
+    won, player = b.winner_exists()
+    assert(not won and player == -1)
 
 def test_board_diagonal_wins():
-	print('Beginning Test for diagonal wins')
-	b = Board(*STANDARD_CONNECT_FOUR_SIZE)
-	for x in range(b.shape()[0]):
-		for y in range(b.shape()[1]):
-		
-			if (x+3 < b.shape()[0] and y+3 < b.shape()[1]):
-				b.board[x][y] = b.board[x+1][y+1] = b.board[x+2][y+2] = b.board[x+3][y+3] = 1
-				won, player = b.winner_exists()
-				assert(won and player == 1)
-				b.clear()
+    print('Beginning Test for diagonal wins')
+    b = Board(*STANDARD_CONNECT_FOUR_SIZE)
+    for x in range(b.shape()[0]):
+        for y in range(b.shape()[1]):
+        
+            if (x+3 < b.shape()[0] and y+3 < b.shape()[1]):
+                b.board[x][y] = b.board[x+1][y+1] = b.board[x+2][y+2] = b.board[x+3][y+3] = 1
+                won, player = b.winner_exists()
+                assert(won and player == 1)
+                b.clear()
 
-			if (x-3 >= 0 and y-3 >= 0):
-				b.board[x][y] = b.board[x-1][y-1] = b.board[x-2][y-2] = b.board[x-3][y-3] = 1
-				won, player = b.winner_exists()
-				assert(won and player == 1)
-				b.clear()
-	won, player = b.winner_exists()
-	assert(not won and player == -1)
+            if (x-3 >= 0 and y-3 >= 0):
+                b.board[x][y] = b.board[x-1][y-1] = b.board[x-2][y-2] = b.board[x-3][y-3] = 1
+                won, player = b.winner_exists()
+                assert(won and player == 1)
+                b.clear()
+    won, player = b.winner_exists()
+    assert(not won and player == -1)
 
 def test_is_full():
-	print('Testing whether the board is full')
-	b = Board(*STANDARD_CONNECT_FOUR_SIZE)
-	for x in range(b.shape()[0]):
-		for y in range(b.shape()[1]):
-			b.apply_action(y, 1)
+    print('Testing whether the board is full')
+    b = Board(*STANDARD_CONNECT_FOUR_SIZE)
+    for x in range(b.shape()[0]):
+        for y in range(b.shape()[1]):
+            b.apply_action(y, 1)
